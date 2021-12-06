101e824d1262a12575bb8d64d4ac1cd0ad105780
Arsalan Khattak <me@khattak.dev>
2021-12-06 21:01:12 +0500

    feat: crud operations

--- controller/notes.js
+++ controller/notes.js
@@ -1,18 +1,76 @@
-exports.getAllNotes = (req, res) => {
-  res.send("(GET) All Notes");
+const Note = require("../models/note");
+exports.getAllNotes = async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.send(notes);
+  } catch (error) {
+    console.log("Something went unexpected!", error.message);
+    res.send("Something went unexpected!");
+  }
 };
-exports.postNote = (req, res) => {
-  console.log(req.body);
-  res.send(`${req.body.title} has been added a note`);
+exports.postNote = async (req, res) => {
+  const { title, description } = req.body;
+
+  try {
+    const note = new Note({
+      title,
+      description,
+    });
+
+    await note.save();
+
+    res.send("Note has been saved!");
+  } catch (error) {
+    console.log("Something went unexpected!", error.message);
+    res.send("Something went unexpected!");
+  }
   //   res.send(req.body.title + "has been added a note");
 };
-exports.getNote = (req, res) => {
+exports.getNote = async (req, res) => {
   console.log(req.params.id);
-  res.send("(GET) Specific Note");
+  const { id } = req.params;
+  try {
+    const note = await Note.findById(id);
+    if (!note) {
+      return res.send("Note doesn't exist");
+    }
+    res.send(note);
+  } catch (error) {
+    console.log("Something went unexpected!", error.message);
+    res.send("Something went unexpected!");
+  }
 };
-exports.updateNote = (req, res) => {
-  res.send("(UPDATE) Update a note");
+exports.updateNote = async (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
+  try {
+    const note = await Note.findById(id);
+    if (!note) {
+      return res.send("Note doesn't exist");
+    }
+
+    note.title = title;
+    note.description = description;
+
+    await note.save();
+
+    res.send("Note has been updated!");
+  } catch (error) {
+    console.log("Something went unexpected!", error.message);
+    res.send("Something went unexpected!");
+  }
 };
-exports.deleteNote = (req, res) => {
-  res.send("(DELETE) Remove a note");
+exports.deleteNote = async (req, res) => {
+  console.log(req.params.id);
+  const { id } = req.params;
+  try {
+    const note = await Note.findByIdAndDelete(id);
+    if (!note) {
+      return res.send("Note doesn't exist");
+    }
+    res.send("Note has been deleted!");
+  } catch (error) {
+    console.log("Something went unexpected!", error.message);
+    res.send("Something went unexpected!");
+  }
 };
